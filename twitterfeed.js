584474608b2f14d27644dc3d4ab4b6ef2f3d73b5
Edwin Martin <edwin@bitstorm.org>
2016-04-17 16:09:28 +0200

    Initial commit

---
+++ twitterfeed.js
@@ -0,0 +1,42 @@
+/**!
+ @preserve twitterFeed 1.0.0
+ @copyright 2016 Edwin Martin
+ @see {@link http://www.bitstorm.org/javascript/}
+ @license MIT
+ */
+var twitterFeed = function () {
+    var _callback, _options;
+
+    return {
+        fetch: function (fetchkey, options, cb) {
+            _options = options;
+            _callback = cb;
+            var script = document.createElement("script");
+            script.src = "https://cdn.syndication.twimg.com/widgets/timelines/" + fetchkey + "?&lang=en&callback=twitterFeed.callback&suppress_response_codes=true&rnd=" + Math.random();
+            document.head.appendChild(script);
+        },
+
+        callback: function (twitterFeed) {
+            var div = document.createElement("div");
+            var html = twitterFeed.body
+            if (_options.images !== true) {
+                html = html.replace(/<img/g, '<x-img');
+            }
+            div.innerHTML = html;
+
+            var tweets = Array.prototype.map.call(div.querySelectorAll(".timeline-Tweet-text"), function(el) {
+                var txt = el.textContent;
+                if (_options.plaintext !== false) {
+                    txt = txt.replace('&', '&amp;').replace('<', '&lt;').replace('>', '&gt;');
+                }
+                if (_options.hyperlinks !== false) {
+                    txt = txt.replace(/(https?:\/\/[a-zA-Z0-9\/\&\?=\[\]%~#;\$\-_\+!\*',":\.\(\)]+[a-zA-Z0-9\/\&\?=\[\]%~#;\$\-_\+!\*',":]{2})/g, '<a href=\'$1\'>$1</a>')
+                        .replace(/@([0-9a-zA-Z_-]+)/g, '<a href=\'http://twitter.com/$1\'>@$1</a>');
+                }
+                return txt;
+            });
+
+            _callback(tweets)
+        }
+    }
+}();
